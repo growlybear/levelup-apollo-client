0e2cff00e177710355290786b371df1d502f985f
Michael Allan <Michael Allan>
2018-08-14 10:53:30 +1000

    Move apollo into App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
 import React, { Component } from 'react'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+import gql from 'graphql-tag'
+
 import logo from './logo.svg'
 import './App.css'
+
+const client = new ApolloClient({
+  uri: 'https://api-apeast.graphcms.com/v1/cjksvdr8u017601burwjyriqm/master'
+})
+
+const query = gql`
+  {
+    posts {
+      title
+      body
+      id
+      status
+      createdAt
+    }
+  }
+`
+
+// NOTE just to test out a query ...
+// client.query({ query }).then(res => console.log(res.data.posts))
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
+      </ApolloProvider>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,36 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import ApolloClient from 'apollo-boost'
-import { ApolloProvider } from 'react-apollo'
-import gql from 'graphql-tag'
 
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
 import './index.css'
 
-const client = new ApolloClient({
-  uri: 'https://api-apeast.graphcms.com/v1/cjksvdr8u017601burwjyriqm/master'
-})
-
-const query = gql`
-  {
-    posts {
-      title
-      body
-      id
-      status
-      createdAt
-    }
-  }
-`
-
-client.query({ query }).then(res => console.log(res.data.posts))
-
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <App />,
   document.getElementById('root')
 )
 registerServiceWorker()
